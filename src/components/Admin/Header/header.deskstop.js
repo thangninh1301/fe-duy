dd61e03388a2b55837cefb145425cb86fb22fcce
Thang <thang@MacBook-Pro-cua-Thang.local>
2024-01-28 21:38:08 +0700

    examination

--- src/components/Admin/Header/header.deskstop.js
+++ src/components/Admin/Header/header.deskstop.js
@@ -52,37 +52,36 @@ const Header = () => {
               >
                 <div onClick={(e) => e.preventDefault()}>
                   <Space>
-                    <Avatar
-                      style={{
-                        backgroundColor: "#33322e",
-                        color: "#fff",
-                      }}
-                      size={32}
-                      icon={<UserOutlined />}
-                    />
-                    <DownOutlined className="tw-text-white" />
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M6 16C6 10.4772 10.4772 6 16 6C21.5228 6 26 10.4772 26 16C26 18.8358 24.8187 21.3968 22.9233 23.2159C21.1271 24.9396 18.6867 26 16 26C13.3133 26 10.8729 24.9396 9.07675 23.2159C7.18129 21.3968 6 18.8358 6 16ZM21.6587 22.4824C21.2578 21.283 20.1245 20.4186 18.7907 20.4186H13.2093C11.8755 20.4186 10.7422 21.283 10.3413 22.4824C11.8544 23.8045 13.8331 24.6047 16 24.6047C18.1669 24.6047 20.1456 23.8045 21.6587 22.4824ZM16 8.7907C13.5597 8.7907 11.5814 10.769 11.5814 13.2093C11.5814 15.6496 13.5597 17.6279 16 17.6279C18.4403 17.6279 20.4186 15.6496 20.4186 13.2093C20.4186 10.769 18.4403 8.7907 16 8.7907Z"
+                            fill="#FAFAFA"/>
+                      <rect opacity="0.1" width="32" height="32" rx="12" fill="white"/>
+                    </svg>
+
+                    <DownOutlined className="tw-text-white"/>
                   </Space>
                 </div>
               </Dropdown>
             ) : (
-              <div className="tw-mr-[12px]">
-                <Link
-                  to="/admin/login"
-                  className="tw-text-white tw-text-[16px]"
-                >
-                  Đăng nhập
-                </Link>
-              </div>
+                <div className="tw-mr-[12px]">
+                  <Link
+                      to="/admin/login"
+                      className="tw-text-white tw-text-[16px]"
+                  >
+                    Đăng nhập
+                  </Link>
+                </div>
             )}
           </div>
         </div>
         <ul className="tw-bg-[#DC1814] tw-flex tw-items-center tw-justify-center !tw-m-0 tw-py-4 !tw-px-0">
           <li className="tw-flex tw-items-center tw-justify-center tw-px-4 2xl:tw-px-[36px] tw-border-0 tw-border-r-[1px] tw-border-solid tw-border-[#fff] tw-py-1 tw-text-[#fff] tw-cursor-pointer">
             <Link
-              to="/admin"
-              className={`tw-text-white tw-text-[16px] ${
-                location?.pathname === "/admin" ? "tw-font-[700]" : ""
-              }`}
+                to="/admin"
+                className={`tw-text-white tw-text-[16px] ${
+                    location?.pathname === "/admin" ? "tw-font-[700]" : ""
+                }`}
             >
               Trang chủ
             </Link>
