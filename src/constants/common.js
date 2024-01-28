28043c1ce3bc92c7fd7a968c3b8a5e5da7fd7b3b
Thang <thang@MacBook-Pro-cua-Thang.local>
2024-01-29 03:18:01 +0700

    examination

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,53 +1,53 @@
 export const BRANCH_LIST = [
-  { value: "TRUMPETER", label: "TRUMPETER" },
-  { value: "MENG", label: "MENG" },
-  { value: "TAMIYA", label: "TAMIYA" },
-  { value: "ZVEZDA", label: "ZVEZDA" },
-  { value: "AMUSING_HOBBY", label: "AMUSING_HOBBY" },
-  { value: "RFM", label: "RFM" },
-  { value: "TIGER_MODELS", label: "TIGER_MODELS" },
-  { value: "AFV_CLUB", label: "AFV_CLUB" },
-  { value: "TAKOM", label: "TAKOM" },
+    {value: `"TRUMPETER"`, label: "TRUMPETER"},
+    {value: `"MENG"`, label: "MENG"},
+    {value: `"TAMIYA"`, label: "TAMIYA"},
+    {value: `"ZVEZDA"`, label: "ZVEZDA"},
+    {value: `"AMUSING_HOBBY"`, label: "AMUSING_HOBBY"},
+    {value: `"RFM"`, label: "RFM"},
+    {value: `"TIGER_MODELS"`, label: "TIGER_MODELS"},
+    {value: `"AFV_CLUB"`, label: "AFV_CLUB"},
+    {value: `"TAKOM"`, label: "TAKOM"},
 ];
 
 export const COUNTRY_LIST = [
-  { value: "Russia", label: "Russia" },
-  { value: "Nato", label: "Nato" },
-  { value: "EastAsia", label: "EastAsia" },
-  { value: "Other", label: "Other" },
+    {value: `"Russia"`, label: "Russia"},
+    {value: `"Nato"`, label: "Nato"},
+    {value: `"EastAsia"`, label: "EastAsia"},
+    {value: ` "Other"`, label: "Other"},
 ];
 
 export const SIZE_LIST = [
-  { value: "35", label: "1/35" },
-  { value: "43", label: "1/43" },
-  { value: "48", label: "1/48" },
-  { value: "72", label: "1/72" },
-  { value: "100", label: "1/100" },
-  { value: "144", label: "1/144" },
-  { value: "350", label: "1/350" },
-  { value: "700", label: "1/700" },
+    {value: `"35"`, label: "1/35"},
+    {value: `"43"`, label: "1/43"},
+    {value: `"48"`, label: "1/48"},
+    {value: `"72"`, label: "1/72"},
+    {value: `"100"`, label: "1/100"},
+    {value: `"144"`, label: "1/144"},
+    {value: `"350"`, label: "1/350"},
+    {value: `"700"`, label: "1/700"},
 ];
 
 export const TYPE_LIST = [
-  { value: "MBT", label: "MBT" },
-  { value: "IFV", label: "IFV" },
-  { value: "Artilerry", label: "Artilerry" },
-  { value: "Radar", label: "Radar" },
-  { value: "Aircraft", label: "Aircraft" },
-  { value: "Soldier", label: "BattlSoldiereship" },
-  { value: "Other", label: "Other" },
+    {value: `"MBT"`, label: "MBT"},
+    {value: `"IFV"`, label: "IFV"},
+    {value: `"Artilerry"`, label: "Artilerry"},
+    {value: `"Radar"`, label: "Radar"},
+    {value: `"Aircraft"`, label: "Aircraft"},
+    {value: `"Soldier"`, label: "BattlSoldiereship"},
+    {value: `"Other"`, label: "Other"},
 ];
 
 export const SORT = [
-  { value: "1", label: "Mặc định" },
-  { value: "2", label: "Từ a -> z" },
-  { value: "3", label: "Từ z -> a" },
+    {value: "1", label: "Mặc định"},
+    {value: "2", label: "Từ a -> z"},
+    {value: "3", label: "Từ z -> a"},
 ];
 
 export const PRICE_LIST = [
-  { value: "1", label: "Dưới 500.000 vnd" },
-  { value: "2", label: "Từ 500.000 đến 1.000.000 vnd" },
-  { value: "3", label: "Từ 1.000.000 đến 1.500.000 vnd" },
-  { value: "4", label: "Từ 1.500.000 đến 2.000.000 vnd" },
+    {value: "1", label: "Dưới 500.000 vnd"},
+    {value: "2", label: "Từ 500.000 đến 1.000.000 vnd"},
+    {value: "3", label: "Từ 1.000.000 đến 1.500.000 vnd"},
+    {value: "4", label: "Từ 1.500.000 đến 2.000.000 vnd"},
 ];
 
